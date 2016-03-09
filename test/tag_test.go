7be06b584ae06c3d5629428d7ff25e5aafd3ad64
Roman Khlystik <roman.khlystik@gmail.com>
2016-03-09 18:02:52 +0000

    Added TestTagsDifferent

--- test/tag_test.go
+++ test/tag_test.go
@@ -4,11 +4,34 @@ import (
 	"testing"
 )
 
-func TestTagTheSame(t *testing.T) {
+func TestTagsDifferent(t *testing.T) {
 	tag := "rocker_integration_test_tag:latest"
 	defer removeImage(tag)
 
-	err := runRockerBuildWithOptions("FROM alpine:latest\nTAG " + tag)
+	err := runRockerBuildWithOptions("FROM alpine:latest\nRUN touch /foobar\nTAG "+tag, "--no-cache")
+	if err != nil {
+		t.Fatalf("Test fail: %v\n", err)
+	}
+
+	sha1, err := getImageShaByName("alpine:latest")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sha2, err := getImageShaByName(tag)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if sha1 == sha2 {
+		t.Fatalf("Sha's of source image and tag are identical but shouldn't. sha1: '%s', sha2: '%s'", sha1, sha2)
+	}
+}
+func TestTagsTheSame(t *testing.T) {
+	tag := "rocker_integration_test_tag:latest"
+	defer removeImage(tag)
+
+	err := runRockerBuildWithOptions("FROM alpine:latest\nTAG "+tag, "--no-cache")
 	if err != nil {
 		t.Fatalf("Test fail: %v\n", err)
 	}
