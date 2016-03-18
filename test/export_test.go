f9d10c5d9f430d3effd6196e39480b1db70e8c0f
Roman Khlystik <roman.khlystik@gmail.com>
2016-03-18 16:33:30 +0000

    Added test for few FROM's

--- test/export_test.go
+++ test/export_test.go
@@ -115,3 +115,49 @@ func TestExportSmolinIssue(t *testing.T) {
 	assert.Nil(t, err, "Can't read file")
 	assert.Equal(t, "first", string(content))
 }
+
+func TestExportSameFileFewFroms(t *testing.T) {
+	dir, err := ioutil.TempDir("/tmp", "rocker_integration_test_export_")
+	assert.Nil(t, err, "Can't create tmp dir")
+	defer os.RemoveAll(dir)
+
+	rockerfile, err := createTempFile("")
+	assert.Nil(t, err, "Can't create temp file")
+	defer os.RemoveAll(rockerfile)
+
+	rockerContentFirst := []byte(`FROM alpine
+								  EXPORT /etc/issue
+
+								  FROM alpine
+								  RUN echo -n "first_few" > /exported_file
+								  EXPORT /exported_file
+
+						 	      FROM alpine
+						 	      MOUNT ` + dir + `:/datadir
+						 	      IMPORT /exported_file /datadir/imported_file`)
+
+	rockerContentSecond := []byte(`FROM alpine
+								  EXPORT /etc/issue
+
+								  FROM alpine
+								  RUN echo -n "second_few" > /exported_file
+								  EXPORT /exported_file`)
+
+	err = ioutil.WriteFile(rockerfile, rockerContentFirst, 0644)
+	assert.Nil(t, err)
+	err = runRockerBuildWithFile(rockerfile, "--reload-cache")
+	assert.Nil(t, err)
+
+	err = ioutil.WriteFile(rockerfile, rockerContentSecond, 0644)
+	assert.Nil(t, err)
+	err = runRockerBuildWithFile(rockerfile, "--reload-cache")
+	assert.Nil(t, err)
+
+	err = ioutil.WriteFile(rockerfile, rockerContentFirst, 0644)
+	assert.Nil(t, err)
+	err = runRockerBuildWithFile(rockerfile)
+	assert.Nil(t, err)
+	content, err := ioutil.ReadFile(dir + "/imported_file")
+	assert.Nil(t, err, "Can't read file")
+	assert.Equal(t, "first_few", string(content))
+}
