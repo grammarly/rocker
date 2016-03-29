acae96be2c66f48b830e33a5e4b91eced46fe1ef
Roman Khlystik <roman.khlystik@gmail.com>
2016-03-29 14:35:40 +0000

    Added PushAndPull test

--- test/pull_test.go
+++ test/pull_test.go
@@ -2,11 +2,12 @@ package tests
 
 import (
 	"github.com/stretchr/testify/assert"
+	"strconv"
 	"testing"
 	"time"
 )
 
-func TestPull(t *testing.T) {
+func TestPullSimple(t *testing.T) {
 	errChan := make(chan error)
 	go func() {
 		errChan <- runRockerPull("alpine")
@@ -15,7 +16,40 @@ func TestPull(t *testing.T) {
 	select {
 	case err := <-errChan:
 		assert.Nil(t, err)
-	case <-time.After(time.Second * 20):
-		t.Fatal("rocker pull timeouted")
+	case <-time.After(time.Second * 30):
+		t.Fatal("rocker pull timeout")
 	}
+}
+
+func TestPushAndPull(t *testing.T) {
+	randomData := strconv.Itoa(int(time.Now().UnixNano() % int64(100000001)))
+
+	tag := "dockerhub.grammarly.io/rocker_integration_test_pull:latest" + randomData
+	defer removeImage(tag)
+
+	err := runRockerBuildWithOptions("FROM alpine\nRUN echo "+randomData+" > /foobar\nPUSH "+tag, "--push")
+	assert.Nil(t, err)
+
+	sha1, err := getImageShaByName(tag)
+	assert.Nil(t, err)
+
+	err = removeImage(tag)
+	assert.Nil(t, err)
+
+	errChan := make(chan error)
+	go func() {
+		errChan <- runRockerPull(tag)
+	}()
+
+	select {
+	case err := <-errChan:
+		assert.Nil(t, err)
+	case <-time.After(time.Second * 30):
+		t.Fatal("rocker pull timeout")
+	}
+
+	sha2, err := getImageShaByName(tag)
+	assert.Nil(t, err)
+
+	assert.Equal(t, sha1, sha2)
 }
